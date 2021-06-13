9621500568afd8d0095ab38a008dfa6791c07e03
R-Rouis <rahoubarouis5@gmail.com>
2021-06-13 19:14:07 +0200

    adding checkpoint API

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{useState,useEffect} from "react"
+import axios from "axios"
+import './App.css';
+import {UserList} from "./UserList";
+
+function App() {
+  const [listOfUSer,setListOfUSer] = useState([])
+  const FetchData = async () =>{
+    const Api = "https://jsonplaceholder.typicode.com/users"
+    const response = await axios.get(Api)
+    setListOfUSer(response.data)
+  }
+  useEffect(()=>{
+    FetchData()
+  },[])
+  return (
+    <div className="App">
+      <nav><i><u>USER-LIST</u></i></nav>
+      <div className={"user-list"}>
+      {listOfUSer.map((elem) => <UserList key={elem.id} email={elem.email} name={elem.name} phone={elem.phone} user={elem.username} />)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
